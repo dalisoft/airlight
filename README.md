16e00e31c603c695f1c5d69b85300ad7ebcca6a7
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-04 18:25:27 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# events
+
+One more events emitter for Node.js and browser
+
+## Features
+
+- Performant
+- Easy
+- UMD compatible
+
+## Installation
+
+We recommend install via `npm` because of it's cache and flat node modules tree
+
+```bash
+npm i @dalisoft/events
+```
+
+then you able to import to Node.js/Browser easily
+
+```js
+// Node.js
+const Events = require("@dalisoft/events");
+
+// Browser
+// window.Events OR Events
+
+// ES6
+import Events from "@dalisoft/events";
+```
+
+## Usage
+
+```js
+const ev = new Events();
+
+ev.on("hello", name => console.log("Hello ", name));
+ev.emit("hello", "world");
+```
+
+## License
+
+MIT
---
+++ event.js
@@ -0,0 +1,121 @@
+(function(factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["@dalisoft/args"], factory);
+  } else if (typeof module !== "undefined" && module.exports) {
+    module.exports = factory(require("@dalisoft/args"));
+  } else if (typeof exports !== "undefined") {
+    exports.default = factory();
+    exports.Events = exports.default;
+  } else if (typeof window !== "undefined" && window.document) {
+    window.Events = factory(window.args);
+  } else {
+    this.Events = factory(this.args);
+  }
+})(function(args) {
+  return class Events {
+    constructor() {
+      Object.defineProperty(this, "___events", {
+        enumerable: false,
+        writable: true,
+        value: {}
+      });
+    }
+    on(name, callback) {
+      const { ___events } = this;
+
+      let _args;
+      if (name.indexOf(":") !== -1) {
+        _args = name.split(":");
+        name = _args.shift();
+
+        callback = args(callback.bind, _args);
+      }
+      if (___events[name] === undefined) {
+        ___events[name] = [];
+      }
+
+      ___events[name].push(callback);
+
+      return this;
+    }
+    once(name, callback) {
+      const { ___events } = this;
+
+      let _args;
+      if (name.indexOf(":") !== -1) {
+        _args = name.split(":");
+        name = _args.shift();
+
+        callback = args(callback.bind, _args);
+      }
+      if (___events[name] === undefined) {
+        ___events[name] = [];
+      }
+
+      const __event = __event[name];
+      __event.push(function onceFn() {
+        args(callback, arguments);
+
+        const i = __event.indexOf(onceFn);
+        if (i !== -1) {
+          __event.splice(i, 1);
+        }
+      });
+
+      return this;
+    }
+    off(name, callback) {
+      if (name !== undefined) {
+        const { ___events } = this;
+
+        if (name.indexOf(":") !== -1) {
+          name = name.split(":").shift();
+        }
+
+        if (___events[name] === undefined) {
+          return this;
+        }
+
+        const __event = ___events[name];
+        if (typeof callback === "function") {
+          const i = __event.indexOf(callback);
+          if (i !== -1) {
+            __event.splice(i, 1);
+          }
+        } else {
+          __event.length = 0;
+        }
+      }
+      return this;
+    }
+    emit(name, ...argsOfFn) {
+      const { ___events } = this;
+
+      let _args;
+      if (name.indexOf(":") !== -1) {
+        _args = name.split(":");
+        name = _args.shift();
+        argsOfFn = _args.concat(argsOfFn);
+      }
+
+      if (___events[name] === undefined) {
+        return this;
+      }
+
+      const __event = ___events[name];
+
+      if (__event.length === 0) {
+        return this;
+      } else if (__event.length === 1) {
+        args(__event[0], argsOfFn);
+        return this;
+      }
+
+      for (let i = 0, len = __event.length; i < len; i++) {
+        args(__event[i], argsOfFn);
+      }
+
+      return this;
+    }
+  };
+});
