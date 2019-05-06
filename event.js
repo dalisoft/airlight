afe89b3d250ee946df24146c9be1a3636cd08bae
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-06 12:10:39 +0500

    fix es6 export

--- event.js
+++ event.js
@@ -5,7 +5,6 @@
     module.exports = factory(require("@dalisoft/args"));
   } else if (typeof exports !== "undefined") {
     exports.default = factory(exports.args || require("@dalisoft/args"));
-    exports.Events = exports.default;
   } else if (typeof window !== "undefined" && window.document) {
     window.Events = factory(window.args);
   } else {
