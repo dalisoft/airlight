17d476fed2386b5f5da5645e1bd13519b46be2d3
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-12 15:38:38 +0500

    add working example

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const processFile = require("./lib");
+
+// Constants
+const params = {};
+const [node, cliFile, ...args] = process.argv;
+
+// Preparing step
+for (let i = 0, len = args.length; i < len; ) {
+  let key = args[i];
+
+  if (key.substr(0, 2) !== "--") {
+    const value = args[i + 1];
+    params[key.substr(1)] = value;
+    i += 2;
+  } else {
+    key = key.substr(2);
+    params[key] = true;
+    i++;
+  }
+}
+
+// Processing step
+processFile(params).catch(err => err);
---
+++ lib.js
@@ -0,0 +1,41 @@
+// Imports
+const fs = require("fs");
+
+const { importsAst, exportsAst, cleanAst } = require("./ast");
+const template = require("./template");
+
+module.exports = params =>
+  new Promise((resolve, reject) => {
+    fs.readFile(params.file, { encoding: "utf-8" }, (err, fileData) => {
+      if (err) {
+        console.log("CLI [Lib-Export]: Error was occured", err);
+        reject(err);
+        return;
+      }
+      const matchImports = importsAst(fileData);
+      const matchExports = exportsAst(fileData);
+      const cleanExports = cleanAst(fileData);
+
+      const processedFileString = template(
+        { imports: matchImports, exports: matchExports },
+        cleanExports,
+        params
+      );
+
+      fs.writeFile(
+        params.output || params.file.replace(/\.js/g, "") + ".processed.js",
+        processedFileString,
+        errWrite => {
+          if (errWrite) {
+            console.log(
+              "CLI [Lib-Export]: Error was occured during save processed file",
+              errWrite
+            );
+            reject(errWrite);
+            return;
+          }
+          resolve();
+        }
+      );
+    });
+  });
