843974789dce00ff282abe46ca3a806fe0263150
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-12 15:52:27 +0500

    update to code-base

---
+++ swipe-gesture.es.js
@@ -0,0 +1,101 @@
+const Swipe = props => {
+  let isPressed = false;
+  let startX = 0;
+  let startY = 0;
+  let isTouch = false;
+  let type = null;
+  let delta = 0;
+
+  const onTouchStartHandler = e => {
+    e =
+      e.pageX !== undefined
+        ? e
+        : e.touches
+        ? e.touches[0]
+        : e.changedTouches
+        ? e.changedTouches[0]
+        : null;
+
+    if (!e) {
+      return false;
+    }
+
+    isPressed = true;
+    startX = e.pageX;
+    startY = e.pageY;
+    if (props.onPointerDown) {
+      props.onPointerDown(e);
+    }
+  };
+  const onTouchMoveHandler = e => {
+    if (isPressed && startX > 0 && startY > 0) {
+      if (e.touches) {
+        if (e.touches.length > 1 || (e.scale && e.scale !== 1)) {
+          return;
+        }
+        e = e.touches[0];
+      }
+      if (!e) {
+        return;
+      }
+
+      if (props.onPointerMove) {
+        props.onPointerMove(e);
+      }
+
+      let dx = e.pageX - startX;
+      let dy = e.pageY - startY;
+
+      let ax = Math.abs(dx);
+      let ay = Math.abs(dy);
+
+      if (ax > ay) {
+        if (props.onPanX) {
+          props.onPanX(dx);
+        }
+        delta = dx;
+        if (dx < 0) {
+          if (props.onSwipeLeft) {
+            type = "onSwipeLeft";
+          }
+        } else if (dx > 0) {
+          if (props.onSwipeRight) {
+            type = "onSwipeRight";
+          }
+        }
+      } else if (ay > ax) {
+        if (props.onPanY) {
+          props.onPanY(dy);
+        }
+        delta = dy;
+        if (dy < 0) {
+          if (props.onSwipeDown) {
+            type = "onSwipeDown";
+          }
+        } else if (dy > 0) {
+          if (props.onSwipeUp) {
+            type = "onSwipeUp";
+          }
+        }
+      }
+    }
+  };
+  const onTouchEndHandler = e => {
+    if (type) {
+      requestAnimationFrame(() => props[type] && props[type](delta));
+      type = null;
+    }
+    if (props.onPointerUp) {
+      requestAnimationFrame(() => props.onPointerUp(e));
+    }
+    isPressed = false;
+  };
+
+  return {
+    onStart: onTouchStartHandler,
+    onMove: onTouchMoveHandler,
+    onEnd: onTouchEndHandler
+  };
+};
+
+export default Swipe;
