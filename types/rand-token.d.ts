f197cb63aee7ae4193b211f388b145c0f41bb420
Davlatjon Shavkatov <dalisoft@mail.ru>
2019-08-28 15:14:27 +0500

    fix missing type

--- types/rand-token.d.ts
+++
@@ -1,4 +0,0 @@
-export function generate(size: any, chars: any): any;
-export function generator(options: any): any;
-export function suid(length: any, epoch: any, prefixLength: any): any;
-export function uid(size: any, chars?: any): any;
