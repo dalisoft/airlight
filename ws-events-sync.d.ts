9ab10f1e74c3ba29d75cec47e7028a1ce2aa2c57
Davlat Shavkatov <dalisoft@mail.ru>
2019-11-10 00:35:43 +0500

    update typings, bump version

--- ws-events-sync.d.ts
+++ ws-events-sync.d.ts
@@ -9,6 +9,7 @@ export class Server extends WebSocketInstance {
   pong(): this;
   close(code?: number, message?: string): this;
   on(name: "message", message: string): this;
+  on(name: "close", code?: number, message?: string): this;
   on(name: string, ...args: any[]): this;
 }
 export class Client extends Server {}
