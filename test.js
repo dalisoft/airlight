d34f5e00e2eabeec48ad37f777c464d3b1bd86df
Davlat Shavkatov <dalisoft@mail.ru>
2019-11-09 22:12:35 +0500

    breaking change: now this package not does same as prev ver and fix tests

--- test.js
+++ test.js
@@ -1,76 +1,106 @@
 import test from "ava";
-import { Server } from "./ws-events-sync";
-import Events from "@dalisoft/events";
+import { Server, Client } from "./ws-events-sync";
 
-class fakeWsServer extends Events {
-  get OPEN() {
-    return "OPEN";
-  }
-  get CLOSED() {
-    return "CLOSED";
-  }
-  get clients() {
-    return new Array(5).fill(null).map(() => new Events()).map(ws => {
-      ws.on('message', msg => console.log('sent to clients', msg))
-      return ws;
-    });
-  }
-  constructor() {
-    super();
+import http from "http";
+import Websocket from "ws";
+import W3CWebSocket from "websocket";
 
-    setTimeout(() => {
-      this.emit("connection");
-    }, 100);
+const createServer = () =>
+  http.createServer((req, res) => {
+    res.write("server test");
+    res.end();
+  });
 
-    this.readyState = this.OPEN;
-  }
-  send(data) {
-    this.clients.forEach(client => {
-      client.emit('message', data);
-    });
-  }
-  close() {
-    this.readyState = this.CLOSED;
-
-    this.emit("close");
-  }
-}
-
-test("Server API test - Basic features", t =>
+test("WebSocket.Server test", t =>
   new Promise(resolve => {
-    t.timeout(1000);
-    t.plan(4);
+    t.timeout(150);
+    t.plan(3);
 
-    const wsWrapper = new Server(new fakeWsServer());
+    const server = createServer();
+    const ws = new Websocket.Server({
+      server,
+      path: "/ws"
+    });
 
-    wsWrapper.on("connection", () => t.pass('Connection passed'));
-    wsWrapper.on("message", () => t.pass('Message passed'));
-    wsWrapper.on("registered-callback", () => t.pass('Custom-callback passed'));
-    wsWrapper.on("close", () => t.pass('Close passed'));
+    ws.on("connection", ws => {
+      const wsWrapper = new Server(ws);
 
-    wsWrapper.emit("registered-callback");
-    wsWrapper.emit("message");
+      t.pass("Connection passed");
 
-    setTimeout(() => {
-      wsWrapper.close();
-      resolve();
-    }, 500);
+      wsWrapper.on("message", data => {
+        t.is(
+          data,
+          "client_message",
+          "Message did not match to text from client"
+        );
+      });
+      wsWrapper.on("close", () => {
+        t.pass("Close passed");
+
+        server.close();
+        resolve();
+      });
+
+      wsWrapper.send("sent to client");
+    });
+
+    server.listen(11115, () => {
+      const { port } = server.address();
+
+      const client = new W3CWebSocket.w3cwebsocket(`ws://localhost:${port}/ws`);
+
+      client.onopen = () => {
+        client.send("client_message");
+      };
+
+      setTimeout(() => {
+        client.close();
+      }, 100);
+    });
   }));
 
-test("Server API test - Should not error 'Excedding max'", t =>
+test("WebSocket.Client test", t =>
   new Promise(resolve => {
-    t.timeout(1000);
-    t.plan(1);
+    t.timeout(150);
+    t.plan(3);
 
-    const ws = new fakeWsServer();
-    const wsWrapper = new Server(ws);
+    const server = createServer();
+    const ws = new Websocket.Server({
+      server,
+      path: "/ws"
+    });
 
-    ws.on("close", () => {
-      t.pass();
-    })
+    ws.on("connection", ws => {
+      ws.send("server_message");
+    });
 
-    setTimeout(() => {
-      wsWrapper.close();
-      resolve();
-    }, 500);
+    server.listen(11156, () => {
+      const { port } = server.address();
+
+      const client = new W3CWebSocket.w3cwebsocket(`ws://localhost:${port}/ws`);
+      const wsClient = new Client(client);
+
+      wsClient.on("open", () => {
+        t.pass("Connection passed");
+        client.send("client_message");
+      });
+
+      wsClient.on("message", message => {
+        t.is(
+          message,
+          "server_message",
+          "Message did not match to text from server"
+        );
+      });
+
+      wsClient.on("close", () => {
+        t.pass("Close passed");
+        server.close();
+        resolve();
+      });
+
+      setTimeout(() => {
+        ws.close();
+      }, 100);
+    });
   }));
--- ws-events-sync.js
+++ ws-events-sync.js
@@ -21,57 +21,14 @@
     this.WSEventsSync.__esModule = true;
   }
 })(function(Events) {
-  let intervalId = null;
-
-  const wsOnlineChecker = (wsClients, ms = 1000, reaction) => {
-    clearInterval(intervalId);
-
-    if (!wsClients) {
-      return false;
-    }
-
-    intervalId = setInterval(() => {
-      // eslint-disable-next-line
-      for (const client in wsClients) {
-        const ws = wsClients[client];
-
-        if (reaction) {
-          reaction(client, ws, ws && ws.ws && ws.ws.readyState === ws.ws.OPEN);
-        }
-        if (!ws || !ws.ws || ws.ws.readyState !== ws.ws.OPEN) {
-          delete wsClients[client];
-        }
-      }
-    }, ms);
-
-    return wsClients;
-  };
-
   const disallowedKeys = ["send", "emit", "on", "once", "off"];
   class Server extends Events {
-    constructor(ws, onlineClientsMap) {
+    constructor(ws, enableQueue) {
       super();
-      this.queue = [];
-      this.ws = ws;
-      this.onlineClientsMap = wsOnlineChecker(
-        onlineClientsMap,
-        2000,
-        (id, client, ready) => {
-          if (ready) {
-            let i = 0;
-            while (i < i < this.queue.length) {
-              const q = this.queue[i];
 
-              if (q.to === id) {
-                client.send(q);
-                this.queue.splice(i, 1);
-              } else {
-                i++;
-              }
-            }
-          }
-        }
-      );
+      this.enableQueue = enableQueue;
+      this.queue = enableQueue ? [] : null;
+      this.ws = ws;
 
       Object.keys(ws)
         .filter(key => disallowedKeys.indexOf(key) === -1)
@@ -107,61 +64,19 @@
           this.emit("message", parseJSON);
         }
       });
-      this.ws.on("connection", e => {
-        this.emit("connection", e);
 
+      if (enableQueue) {
         let i = 0;
-        while (i < i < this.queue.length) {
-          const q = this.queue[i];
-
-          if (q.to === "$self") {
-            ws.send(q.data);
-            this.queue.splice(i, 1);
-          } else {
-            i++;
-          }
+        while (i < this.queue.length) {
+          ws.send(this.queue[i]);
+          this.queue.splice(i, 1);
         }
-      });
-      this.ws.on('close', e => {
-        this.emit('close', e);
+      }
+
+      this.ws.on("close", e => {
+        this.emit("close", e);
       });
       return this;
-    }
-    sendToUser(id, data, reaction) {
-      const targetUser = this.onlineClientsMap[id];
-
-      if (
-        targetUser &&
-        targetUser.ws &&
-        targetUser.ws.readyState === targetUser.ws.OPEN
-      ) {
-        targetUser.send(data);
-        if (typeof reaction === "function") {
-          reaction(targetUser, data);
-        }
-      } else {
-        this.queue.push({
-          to: id,
-          data,
-          reaction
-        });
-      }
-      return this;
-    }
-    attachUser(id) {
-      if (this.isOnline(id)) {
-        const instance = this.onlineClientsMap[id];
-        instance.close();
-      }
-      this.onlineClientsMap[id] = this;
-      return this;
-    }
-    detachUser(id) {
-      delete this.onlineClientsMap[id];
-      return this;
-    }
-    isOnline(id) {
-      return this.onlineClientsMap[id] !== undefined;
     }
     ping() {
       if (this.ws.readyState === this.ws.OPEN) {
@@ -177,22 +92,17 @@
       if (typeof data !== "string") {
         data = JSON.stringify(data);
       }
-      if (this.ws.readyState !== this.ws.OPEN) {
-        this.queue.push({
-          to: "$self",
-          data
-        });
+      if (this.enableQueue && this.ws.readyState !== this.ws.OPEN) {
+        this.queue.push(data);
         return this;
       }
       this.ws.send(data);
       return this;
     }
     emit(name, ...args) {
-      const isTypicalEvents = name === 'connection' || name === 'message' || name === 'close';
+      const isTypicalEvents = name === "message" || name === "close";
 
-      if (
-        isTypicalEvents || this.___events[name]
-      ) {
+      if (isTypicalEvents || this.___events[name]) {
         if (!isTypicalEvents && !this.___events[name]) {
           return this;
         }
@@ -212,10 +122,11 @@
     }
   }
   class Client extends Events {
-    constructor(ws) {
+    constructor(ws, enableQueue) {
       super();
 
-      this.queue = [];
+      this.enableQueue = enableQueue;
+      this.queue = enableQueue ? [] : null;
       this.ws = ws;
 
       Object.keys(ws)
@@ -258,15 +169,11 @@
       this.ws.onopen = e => {
         this.emit("open", e);
 
-        let i = 0;
-        while (i < i < this.queue.length) {
-          const q = this.queue[i];
-
-          if (q.to === "$self") {
-            ws.send(q.data);
+        if (enableQueue) {
+          let i = 0;
+          while (i < this.queue.length) {
+            ws.send(this.queue[i]);
             this.queue.splice(i, 1);
-          } else {
-            i++;
           }
         }
       };
@@ -292,23 +199,21 @@
       if (typeof data !== "string") {
         data = JSON.stringify(data);
       }
-      if (this.ws.readyState !== this.ws.OPEN) {
-        this.queue.push({
-          to: "$self",
-          data
-        });
+      if (this.enableQueue && this.ws.readyState !== this.ws.OPEN) {
+        this.queue.push(data);
         return this;
       }
       this.ws.send(data);
       return this;
     }
     emit(name, ...args) {
-      const isTypicalEvents = name === 'open' || name === 'message' || name === 'error' || name === 'close';
+      const isTypicalEvents =
+        name === "open" ||
+        name === "message" ||
+        name === "error" ||
+        name === "close";
 
-      if (
-        isTypicalEvents ||
-        this.___events[name]
-      ) {
+      if (isTypicalEvents || this.___events[name]) {
         if (!isTypicalEvents && !this.___events[name]) {
           return this;
         }
