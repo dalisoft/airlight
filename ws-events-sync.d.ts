c3c51093f4fe77e5808dc308396b2a5f04144964
Davlat Shavkatov <dalisoft@mail.ru>
2019-11-16 17:34:39 +0500

    update deps reflecting to events

--- ws-events-sync.d.ts
+++ ws-events-sync.d.ts
@@ -1,15 +1,15 @@
 import Events from "@dalisoft/events";
 
-export class WebSocketInstance extends Events {
-  close(code?: number, message?: string);
-}
-export class Server extends WebSocketInstance {
-  constructor(ws: WebSocketInstance, enableQueue?: boolean);
+export class Server extends Events {
+  constructor(ws: Events, enableQueue?: boolean);
   ping(): this;
   pong(): this;
   close(code?: number, message?: string): this;
   on(name: "message", message: string): this;
   on(name: "close", code?: number, message?: string): this;
   on(name: string, ...args: any[]): this;
+  close(code?: number, message?: string);
 }
-export class Client extends Server {}
+export class Client extends Server {
+  constructor(ws, enableQueue?: boolean);
+}
