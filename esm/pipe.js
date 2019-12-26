85bd19d56964e2bafd94b55ebd1bbad655bc70f0
Davlatjon Shavkatov <dalisoft@mail.ru>
2019-12-26 16:32:03 +0500

    chore(code): improve code-quality

--- esm/pipe.js
+++ esm/pipe.js
@@ -1,11 +1,14 @@
+/** global: request, response */
 import { reqBodyPipe, reqPipe, resPipe } from "./utils/index.js";
 
 export default ({ req: request = true, res: response = true } = {}) => {
   return (res, req) => {
-    if (typeof request === "boolean" && request === true && req.body) {
-      req.pipe = reqBodyPipe;
-    } else if (request && req.stream) {
-      req.pipe = reqPipe;
+    if (typeof request === "boolean" && request === true) {
+      if (req.body) {
+        req.pipe = reqBodyPipe;
+      } else if (req.stream) {
+        req.pipe = reqPipe;
+      }
     }
     if (typeof response === "boolean" && response === true) {
       res._req = req;
