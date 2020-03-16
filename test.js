eb9bcf826c22c863be831178f6bc8027342ebf2c
dalisoft <dalisoft@mail.ru>
2020-03-16 23:13:26 +0500

    tests: fixing ava update

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-import test from "ava";
-const asBatch = require("./dist/cjs/as-batch");
+const test = require("ava");
+const asBatch = require(".");
 
 const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
