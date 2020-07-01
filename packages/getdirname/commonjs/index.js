b7d816c3897edf816d40d2f09b7a7cf96e1d0619
dalisoft <dalisoft@mail.ru>
2020-07-01 21:49:18 +0500

    fix(getdirname): fix getting real path fixes

--- packages/getdirname/commonjs/index.js
+++ packages/getdirname/commonjs/index.js
@@ -2,13 +2,30 @@ module.exports = function getdirname(){
   try {
     throw new Error('__get__dirname__')
   } catch (e) {
-    var currStackTrace = e.stack.split("at ")[2];
-    var currStack = currStackTrace.match(/\((.*)\:[0-9]/);
-    var matchCurrStack = currStack && currStack[1];
+    var currStackTrace = e.stack.split("at ");
 
-    var lastSlashIndex = matchCurrStack.lastIndexOf('/');
-    var realdirname = matchCurrStack.substr(0, lastSlashIndex);
+    var stackTraceRun = currStackTrace.shift();
+    while (true) {
+      if (currStackTrace[0].indexOf(' (/') !== -1) {
+        stackTraceRun = currStackTrace.shift();
+      } else {
+        break;
+      }
+    }
 
-    return realdirname
+    if (!stackTraceRun) {
+      return null;
+    }
+
+    var lastSlashIndex = stackTraceRun.lastIndexOf('/');
+    var firstBrace = stackTraceRun.indexOf(' (/');
+
+    if (firstBrace !== -1 && lastSlashIndex !== -1) {
+      firstBrace += 2;
+      var realdirname = stackTraceRun.substr(firstBrace, lastSlashIndex - firstBrace);
+      return realdirname
+    }
+
+    return null;
   }
 }
--- packages/getdirname/esm.js
+++ packages/getdirname/esm.js
@@ -2,11 +2,27 @@ export default function getdirname(){
   try {
     throw new Error('__get__dirname__')
   } catch (e) {
-    const currStackTrace = e.stack.split("at ")[2];
+    const currStackTrace = e.stack.split("at ");
 
-    const lastSlashIndex = currStackTrace.lastIndexOf('/');
-    const realdirname = currStackTrace.substr(0, lastSlashIndex);
+    let stackTraceRun = currStackTrace.shift();
+    while (true) {
+      if (currStackTrace[0].indexOf('file://') !== -1) {
+        stackTraceRun = currStackTrace.shift();
+      } else {
+        break;
+      }
+    }
 
+    if (!stackTraceRun) {
+      return null;
+    }
+
+    const lastSlashIndex = stackTraceRun.lastIndexOf('/');
+
+    if (lastSlashIndex !== -1) {
+    const realdirname = stackTraceRun.substr(0, lastSlashIndex);
     return realdirname.replace('file://', '')
   }
+
+  return null;
 }
