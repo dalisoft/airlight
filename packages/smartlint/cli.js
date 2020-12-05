44c37277e73a86823e3593ab477918cdc1c9eb46
dalisoft <dalisoft@mail.ru>
2020-12-05 15:51:05 +0500

    chore: improvements and fixes

--- packages/smartlint/cli.js
+++ packages/smartlint/cli.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+/* eslint-disable no-console, node/no-unpublished-require */
 
 const smartlint = require('.');
-const { execAsync } = require('./util');
 const util = require('./util');
-const [bin, callee, ...args] = process.argv;
+
+const [, , ...args] = process.argv;
 const path = args.pop();
 
 const options = {};
@@ -15,6 +16,9 @@ const invalid = (code) => {
     case 'invalid-option': {
       message =
         'Invalid option, please use like `smartlint --linters=eslint,prettier ./src';
+      break;
+    }
+    default: {
       break;
     }
   }
@@ -35,13 +39,17 @@ for (let i = 0; i < args.length; i += 1) {
 const lintCommand = smartlint(options.linters, path);
 
 if (lintCommand) {
-  execAsync(lintCommand)
+  util
+    .execAsync(lintCommand)
     .then(console.log)
     .catch((err) => {
       const [, error, ...messages] = err.message.split('\n\n');
 
       util.error(error);
 
-      messages.forEach((message) => console.log(message));
+      // eslint-disable-next-line security-node/detect-crlf
+      messages.forEach((logMessage) => console.log(logMessage));
     });
 }
+
+return null;
