98c3d2173540b4c227d2c1e460241928e35e5a36
dalisoft <dalisoft@mail.ru>
2020-12-20 03:19:31 +0500

    chore: remove bash script

--- lint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-yarn run eslint --color .
-# yarn run stylelint --color .
-yarn run markdownlint-cli2 {*.md,**/*.md} "#node_modules"
-yarn run htmlhint .
-yarn run spectral lint --ignore-unknown-format .
-# yarn run dockerfilelint Dockerfile
-yarn run prettier -c .
