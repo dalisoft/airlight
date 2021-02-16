cf4af94f684bee3a028515948a779a9861ddcaac
dalisoft <dalisoft@mail.ru>
2021-02-17 00:02:20 +0500

    fix(eslint-config-airlight-node): add necessary rule for nodejs

--- packages/eslint-config-node/config.js
+++ packages/eslint-config-node/config.js
@@ -8,6 +8,20 @@ module.exports = Object.assign(baseConfig, {
   plugins: baseConfig.plugins.concat(['security-node']),
   overrides: baseConfig.overrides.concat([
     {
+      files: ['*.js'],
+      rules: {
+        'import/extensions': [
+          'error',
+          'never',
+          {
+            js: 'always',
+            json: 'always',
+            wasm: 'always'
+          }
+        ]
+      }
+    },
+    {
       files: ['*.cjs'],
       rules: {
         '@typescript-eslint/no-var-requires': 'off'
