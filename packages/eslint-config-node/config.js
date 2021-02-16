561695b905e35322209fb997f885fbafc484e708
dalisoft <dalisoft@mail.ru>
2021-02-16 23:56:35 +0500

    fix(eslint-config-airlight-node): reduce config for simplicity

--- packages/eslint-config-node/config.js
+++ packages/eslint-config-node/config.js
@@ -1,108 +1,31 @@
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 2019
-  },
-  extends: [
-    'eslint:recommended',
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:promise/recommended',
+const baseConfig = require('eslint-config-airlight-base');
+
+module.exports = Object.assign(baseConfig, {
+  extends: baseConfig.extends.concat([
     'plugin:node/recommended',
-    'plugin:security-node/recommended',
-    'prettier',
-    'prettier/@typescript-eslint'
-  ],
-  plugins: [
-    '@typescript-eslint',
-    'eslint-comments',
-    'promise',
-    'import',
-    'optimize-regex',
-    'security-node',
-    'no-secrets'
-  ],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': ['off'],
-    '@typescript-eslint/explicit-module-boundary-types': ['off'],
-    '@typescript-eslint/indent': ['off'],
-    '@typescript-eslint/comma-dangle': ['error', 'never'],
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/no-non-null-asserted-optional-chain': ['error'],
-    '@typescript-eslint/restrict-plus-operands': ['error'],
-    'operator-linebreak': ['off', 'after'],
-    'prefer-template': 'error',
-    'no-underscore-dangle': ['off'],
-    'no-useless-concat': ['error'],
-    'no-console': ['error'],
-    'no-template-curly-in-string': 'error',
-    quotes: [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true
-      }
-    ],
-    'no-shadow': 'error',
-    'import/prefer-default-export': ['error']
-  },
-  overrides: [
-    {
-      // enable the rule specifically for TypeScript files
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': ['error'],
-        '@typescript-eslint/explicit-module-boundary-types': ['error'],
-        '@typescript-eslint/quotes': [
-          'error',
-          'single',
-          {
-            avoidEscape: true,
-            allowTemplateLiterals: true
-          }
-        ],
-        '@typescript-eslint/no-shadow': 'error',
-        quotes: 'off',
-        'no-shadow': 'off'
-      }
-    },
-    {
-      files: ['*.js'],
-      rules: {
-        'import/extensions': [
-          'error',
-          'never',
-          {
-            js: 'always',
-            json: 'always',
-            wasm: 'always'
-          }
-        ]
-      }
-    },
+    'plugin:security-node/recommended'
+  ]),
+  plugins: baseConfig.plugins.concat(['security-node']),
+  overrides: baseConfig.overrides.concat([
     {
       files: ['*.cjs'],
       rules: {
         '@typescript-eslint/no-var-requires': 'off'
       }
     }
-  ],
+  ]),
   settings: {
     node: {
-      tryExtensions: [
+      tryExtensions: baseConfig.settings.node.tryExtensions.concat([
         '.ts',
         '.js',
         '.cjs',
         '.mjs',
-        '.tsx',
-        '.jsx',
         '.d.ts',
         '.html',
         '.md',
         '.json'
-      ]
+      ])
     }
   }
-};
+});
