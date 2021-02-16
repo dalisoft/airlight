97dfa40b0f63f73ca6d8411dd75ff5a9a0036a96
dalisoft <dalisoft@mail.ru>
2021-02-16 23:38:27 +0500

    fix(eslint-config-airlight-base): introduce base for everywhere

---
+++ packages/eslint-config-base/README.md
@@ -0,0 +1,54 @@
+# eslint-config-airlight-base
+
+ESLint configure for base focused on code quality,
+security and performance on top of
+[eslint-config-airbnb-typescript/base](https://github.com/iamturns/eslint-config-airbnb-typescript)
+
+## Installation
+
+```bash
+npm install eslint-config-airlight-base --save-dev
+# or
+yarn add eslint-config-airlight-base -D
+```
+
+## Peer dependencies
+
+- <https://github.com/microsoft/TypeScript>
+
+## Dependencies
+
+- <https://github.com/iamturns/eslint-config-airbnb-typescript>
+- <https://github.com/typescript-eslint/typescript-eslint>
+- <https://github.com/prettier/eslint-config-prettier>
+- <https://github.com/benmosher/eslint-plugin-import>
+- <https://github.com/nickdeis/eslint-plugin-no-secrets>
+- <https://github.com/dolsem/eslint-plugin-filename-rules>
+
+## Dependencies reason
+
+- eslint-config-airbnb-typescript - Main config
+- @typescript-eslint/eslint-plugin - Config for TypeScript Node.js codebase
+- eslint-config-prettier - Prettier config
+- eslint-plugin-import - ESLint plugin with rules that help validate proper imports
+- eslint-plugin-no-secrets - secrets/credentials finder/matched plug-in for ESLint
+
+## Usage
+
+`.eslintrc`
+
+```json
+{
+  "extends": "airlight-base"
+}
+```
+
+## Rules
+
+We customized following rules.
+
+Change these [configs](./config.js) by your needs
+
+## License
+
+MIT
---
+++ packages/eslint-config-base/config.js
@@ -0,0 +1,130 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'eslint-comments',
+    'promise',
+    'import',
+    'optimize-regex',
+    'no-secrets',
+    'filename-rules'
+  ],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['function', 'method'],
+        format: ['strictCamelCase'],
+        leadingUnderscore: 'allow'
+      },
+      {
+        selector: ['property'],
+        format: ['snake_case']
+      }
+    ],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    '@typescript-eslint/indent': ['off'],
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-non-null-asserted-optional-chain': ['error'],
+    '@typescript-eslint/restrict-plus-operands': ['error'],
+    'operator-linebreak': ['off', 'after'],
+    'prefer-template': 'error',
+    'import/prefer-default-export': ['error'],
+    'filename-rules/match': ['error', 'kebab-case'],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    complexity: ['error', { max: 3 }],
+    'max-depth': ['error', { max: 2 }],
+    'max-nested-callbacks': ['error', { max: 2 }],
+    'max-lines-per-function': [
+      'error',
+      { max: 20, skipBlankLines: true, skipComments: true, IIFEs: true }
+    ],
+    'max-lines': [
+      'error',
+      { max: 160, skipBlankLines: true, skipComments: true }
+    ],
+    'no-underscore-dangle': ['off'],
+    'no-useless-concat': ['error'],
+    'no-console': ['error'],
+    'no-template-curly-in-string': 'error',
+    'no-shadow': 'error'
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+        '@typescript-eslint/explicit-module-boundary-types': ['error'],
+        '@typescript-eslint/quotes': [
+          'error',
+          'single',
+          {
+            avoidEscape: true,
+            allowTemplateLiterals: true
+          }
+        ],
+        '@typescript-eslint/no-shadow': 'error',
+        quotes: 'off',
+        'no-shadow': 'off'
+      }
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        'import/extensions': [
+          'error',
+          'never',
+          {
+            js: 'always',
+            json: 'always',
+            wasm: 'always'
+          }
+        ]
+      }
+    },
+    {
+      files: ['*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ],
+  settings: {
+    node: {
+      tryExtensions: [
+        '.ts',
+        '.js',
+        '.cjs',
+        '.mjs',
+        '.tsx',
+        '.jsx',
+        '.d.ts',
+        '.html',
+        '.md',
+        '.json'
+      ]
+    }
+  }
+};
