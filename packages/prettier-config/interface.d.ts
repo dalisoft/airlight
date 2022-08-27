15875c22cc5eb018063b3b93c56a134ff13acff7
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-08-27 20:28:42 +0500

    fix(prettier-config-airlight): interface fix

--- packages/prettier-config/interface.d.ts
+++ packages/prettier-config/interface.d.ts
@@ -1,13 +1,22 @@
 declare class PrettierConfig {
   static singleQuote: true;
+
   static printWidth: 80;
+
   static trailingComma: 'none';
+
   static arrowParens: 'always';
+
   static bracketSpacing: true;
+
   static bracketSameLine: true;
+
   static useTabs: false;
+
   static embeddedLanguageFormatting: 'auto';
+
   static singleAttributePerLine: true;
+
   static endOfLine: 'lf';
 }
 
