bb450ce6f75c39ecc63331559bccf8260f2d364d
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-01-04 19:26:11 +0500

    fix(airlight-resolve): add typings
    docs(airlight-resolve): update bench

--- packages/airlight-resolve/bench/bench-1.cjs
+++ packages/airlight-resolve/bench/bench-1.cjs
@@ -4,15 +4,31 @@
 const resolve = require('../resolve.cjs');
 
 const ITERATEE = 1_000_000;
+const CWD = process.cwd();
 
-console.time('nodejs resolve');
-for (let i = 0; i < ITERATEE; i++) {
-  require.resolve(`${process.cwd()}/package.json`);
-}
-console.timeEnd('nodejs resolve');
+const input = [
+  `${CWD}/package.json`,
+  'eslint-config-airlight-node',
+  'oxc-resolver',
+  `${CWD}/node_modules/biome-config-airlight`
+];
 
-console.time('airlight-resolve');
-for (let i = 0; i < ITERATEE; i++) {
-  resolve(`${process.cwd()}/package.json`);
+console.time('nodejs resolve total');
+for (const file of input) {
+  console.time(`nodejs resolve ${file}`);
+  for (let i = 0; i < ITERATEE; i++) {
+    require.resolve(file);
+  }
+  console.timeEnd(`nodejs resolve ${file}`);
 }
-console.timeEnd('airlight-resolve');
+console.timeEnd('nodejs resolve total');
+
+console.time('airlight-resolve total');
+for (const file of input) {
+  console.time(`airlight-resolve ${file}`);
+  for (let i = 0; i < ITERATEE; i++) {
+    resolve(file);
+  }
+  console.timeEnd(`airlight-resolve ${file}`);
+}
+console.timeEnd('airlight-resolve total');
---
+++ packages/airlight-resolve/interface.d.ts
@@ -0,0 +1,3 @@
+declare function resolve(...args: string[]): string | void;
+
+export = resolve;
