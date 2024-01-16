fb59378526a0d7a4c783b8c28b4d52ddbc0d4b43
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-01-17 00:43:25 +0500

    fix(airlight-resolve): improve compatibility
    feat(airlight-resolve): now supports `create` method

--- packages/airlight-resolve/resolve.cjs
+++ packages/airlight-resolve/resolve.cjs
@@ -1,20 +1,35 @@
 const { ResolverFactory } = require('oxc-resolver');
 
-const cwd = process.cwd();
-const factory = new ResolverFactory({
+const defaultOptions = {
   preferAbsolute: true,
   fullySpecified: false,
   mainFields: ['main', 'module', 'browser'],
   extensions: ['.ts', '.js', '.d.ts', '.html', '.md', '.json']
-});
-const cache = {};
-
-module.exports = function customResolve(...args) {
-  const key = args.length < 2 ? args[0] : args.join('/');
-
-  if (!cache[key]) {
-    cache[key] = factory.sync(cwd, key).path;
-  }
-
-  return cache[key];
 };
+
+const create = (opts = {}) => {
+  const options = Object.assign(
+    {
+      syncRoot: process.cwd(),
+      cache: false
+    },
+    defaultOptions,
+    opts
+  );
+  const factory = new ResolverFactory(options);
+  const cache = new Map();
+
+  // eslint-disable-next-line complexity
+  return (...args) => {
+    const key = args.length < 2 ? args[0] : args.join('/');
+
+    if (options.cache === false || !cache.has(key)) {
+      cache.set(key, factory.sync(options.syncRoot || args[0], key).path);
+    }
+
+    return cache.get(key);
+  };
+};
+
+module.exports = create();
+module.exports.create = create;
