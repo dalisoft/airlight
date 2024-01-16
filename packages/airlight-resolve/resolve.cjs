2e5d4505f53a3057ed65e638ec256a4bde41b65e
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-01-17 00:21:35 +0500

    feat(airlight-resolve): implement resolves path caching

--- packages/airlight-resolve/resolve.cjs
+++ packages/airlight-resolve/resolve.cjs
@@ -1,11 +1,20 @@
 const { ResolverFactory } = require('oxc-resolver');
 
+const cwd = process.cwd();
 const factory = new ResolverFactory({
   preferAbsolute: true,
   fullySpecified: false,
   mainFields: ['main', 'module', 'browser'],
   extensions: ['.ts', '.js', '.d.ts', '.html', '.md', '.json']
 });
+const cache = {};
+
 module.exports = function customResolve(...args) {
-  return factory.sync(args[0], args.length < 2 ? args[0] : args.join('/')).path;
+  const key = args.length < 2 ? args[0] : args.join('/');
+
+  if (!cache[key]) {
+    cache[key] = factory.sync(cwd, key).path;
+  }
+
+  return cache[key];
 };
