3e2496200aa870528de28a95d53c88f1ba78152e
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-01-17 00:43:34 +0500

    docs(airlight-resolve): update example

--- packages/airlight-resolve/examples/basic.mjs
+++ packages/airlight-resolve/examples/basic.mjs
@@ -1,4 +1,7 @@
 import resolve from '../resolve.mjs';
 
 // eslint-disable-next-line no-console
-console.log([process.cwd()], resolve(process.cwd()));
+console.log(
+  [`${process.cwd()}/package.json`],
+  resolve(process.cwd(), 'package.json')
+);
