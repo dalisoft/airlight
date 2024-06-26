4b275d60cad474976db4960d2219acca0557d27e
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-06-26 23:06:17 +0500

    fix(eslint-config-airlight-node): fix typings and remove unused dep

--- packages/eslint-config-node/legacy.cjs
+++ packages/eslint-config-node/legacy.cjs
@@ -1,16 +1,16 @@
 // @ts-check
-const { defineConfig } = require('eslint-define-config');
-
-// @ts-expect-error It is javascript config
 const baseConfig = require('eslint-config-airlight-base/legacy');
 const baseConfigSettings = require('eslint-config-airlight-base/settings.cjs');
 
-module.exports = defineConfig({
+// @ts-expect-error What it needs idk
+/** @type {import('eslint-define-config').ESLintConfig} */
+module.exports = {
   ...baseConfig,
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module'
   },
+  // @ts-expect-error It should work but does not
   extends: baseConfig.extends.concat(['plugin:n/recommended']),
   env: {
     es2015: true,
@@ -23,6 +23,7 @@ module.exports = defineConfig({
     ...baseConfig.rules,
     'n/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }]
   },
+  // @ts-expect-error It should work but does not
   overrides: baseConfig.overrides.concat([
     {
       files: ['*.js'],
@@ -46,4 +47,4 @@ module.exports = defineConfig({
     }
   ]),
   settings: baseConfigSettings
-});
+};
