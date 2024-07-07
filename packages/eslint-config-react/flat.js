bd7d3ed3f5d3736494d21ff277b53f6894f18a19
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-07-07 22:28:34 +0500

    feat(eslint-config-airlight-react): add `oxlint` config

--- packages/eslint-config-react/flat.js
+++ packages/eslint-config-react/flat.js
@@ -3,6 +3,7 @@ const baseConfig = require('eslint-config-airlight-base');
 const baseConfigSettings = require('eslint-config-airlight-base/settings.cjs');
 const tsFiles = require('eslint-config-airlight-base/overrides/ts-files.cjs');
 const globals = require('globals');
+const rules = require('./rules.cjs');
 
 // Modify it for performance and code clean reason
 if (tsFiles?.[0]?.files) {
@@ -23,60 +24,7 @@ module.exports = [
         ...globals.jest
       }
     },
-    rules: {
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          selector: ['function', 'method'],
-          format: ['strictCamelCase'],
-          leadingUnderscore: 'forbid'
-        },
-        {
-          selector: 'property',
-          format: ['snake_case', 'strictCamelCase'],
-          leadingUnderscore: 'forbid'
-        },
-        {
-          selector: 'objectLiteralProperty',
-          format: ['StrictPascalCase'],
-          leadingUnderscore: 'forbid'
-        },
-        {
-          selector: 'variable',
-          format: ['strictCamelCase'],
-          leadingUnderscore: 'forbid'
-        },
-        {
-          selector: 'variable',
-          types: ['boolean', 'number'],
-          modifiers: ['const'],
-          format: ['UPPER_CASE', 'strictCamelCase'],
-          leadingUnderscore: 'forbid'
-        },
-        {
-          selector: 'typeLike',
-          format: ['StrictPascalCase'],
-          leadingUnderscore: 'forbid'
-        },
-        {
-          selector: 'interface',
-          format: ['PascalCase'],
-          leadingUnderscore: 'forbid',
-          custom: {
-            regex: '^I[A-Z]',
-            match: true
-          }
-        }
-      ],
-      'react/jsx-pascal-case': ['error'],
-      'react/jsx-no-useless-fragment': ['error'],
-      'react/no-deprecated': ['error'],
-      'react/jsx-filename-extension': [
-        'error',
-        { extensions: ['.jsx', '.tsx'] }
-      ],
-      'react-refresh/only-export-components': 'warn'
-    },
+    rules,
     settings: {
       node: {
         tryExtensions: [
