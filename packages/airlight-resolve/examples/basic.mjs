bf7dec12b10d303c2ad8b80c83a2d655eb55322e
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2025-02-25 09:39:05 +0500

    fix(airlight-resolve): update example

--- packages/airlight-resolve/examples/basic.mjs
+++ packages/airlight-resolve/examples/basic.mjs
@@ -2,7 +2,7 @@
 /* eslint-disable no-console */
 import resolve from '../resolve.js';
 
-// biome-ignore lint/suspicious/noConsoleLog: <explanation>
+// biome-ignore lint/suspicious/noConsole: For example it's OK to use console
 console.log(
   [`${process.cwd()}/package.json`],
   resolve(process.cwd(), 'package.json')
