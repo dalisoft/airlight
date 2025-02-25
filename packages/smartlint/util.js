3afcc9918f420dc3481f8879ca745488a85c79e7
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2025-02-25 09:39:22 +0500

    chore(smartlint): update biome rule

--- packages/smartlint/util.js
+++ packages/smartlint/util.js
@@ -9,15 +9,15 @@ const fgYellow = '\x1b[33m';
 const fgBlue = '\x1b[34m';
 
 const logError = (/** @type {string[]} */ ...args) => {
-  // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+  // biome-ignore lint/suspicious/noConsole: <explanation>
   console.log(fgRed, ...args, fgReset);
 };
 const logWarn = (/** @type {string[]} */ ...args) => {
-  // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+  // biome-ignore lint/suspicious/noConsole: <explanation>
   console.log(fgYellow, ...args, fgReset);
 };
 const logDebug = (/** @type {string[]} */ ...args) => {
-  // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+  // biome-ignore lint/suspicious/noConsole: <explanation>
   console.log(fgBlue, ...args, fgReset);
 };
 
