ceed8b5a0dc12883e12f536ecfafa4ff690b404e
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2025-09-14 00:23:29 +0500

    chore: update build scripts

--- packages/eslint-config-base/scripts/build-oxlint.js
+++ packages/eslint-config-base/scripts/build-oxlint.js
@@ -1,5 +1,5 @@
-import fs from "node:fs/promises";
-import legacy from "../legacy.js";
+import fs from 'node:fs/promises';
+import legacy from '../legacy.js';
 
 const { env, globals = {}, plugins, rules, settings = {} } = legacy;
 
@@ -9,25 +9,25 @@ const _rules = {};
 const _settings = {};
 
 const unsupportedPlugins = [
-	"no-secrets",
-	"optimize-regex",
-	"@eslint-community",
+  'no-secrets',
+  'optimize-regex',
+  '@eslint-community'
 ];
 const remaps = [
-	{ old: "import-x", offset: 8, replace: "import", skip: false },
-	{ old: "n", offset: 1, replace: "node", skip: false },
-	...unsupportedPlugins.map((plugin) => ({
-		old: plugin,
-		offset: plugin.length,
-		replace: null,
-		skip: true,
-	})),
+  { old: 'import-x', offset: 8, replace: 'import', skip: false },
+  { old: 'n', offset: 1, replace: 'node', skip: false },
+  ...unsupportedPlugins.map((plugin) => ({
+    old: plugin,
+    offset: plugin.length,
+    replace: null,
+    skip: true
+  }))
 ];
 const remapRules = remaps.map((remap) => ({
-	...remap,
-	old: `${remap.old}/`,
-	offset: remap.offset + 1,
-	replace: `${remap.replace}/`,
+  ...remap,
+  old: `${remap.old}/`,
+  offset: remap.offset + 1,
+  replace: `${remap.replace}/`
 }));
 
 /**
@@ -35,43 +35,42 @@ const remapRules = remaps.map((remap) => ({
  * @param {Record<string, string>} oldMap
  */
 const remapFn = (oldMap, newMap) => {
-	loop1: for (const key in oldMap) {
-		for (const { old, offset, replace, skip } of remapRules) {
-			if (key.substring(0, offset) === old && skip) {
-				continue loop1;
-			} else if (key.substring(0, offset) === old) {
-				newMap[replace + key.substring(offset)] = oldMap[key];
-				continue loop1;
-			}
-		}
-		newMap[key] = oldMap[key];
-	}
+  loop1: for (const key in oldMap) {
+    for (const { old, offset, replace, skip } of remapRules) {
+      if (key.substring(0, offset) === old && skip) {
+        continue loop1;
+      } else if (key.substring(0, offset) === old) {
+        newMap[replace + key.substring(offset)] = oldMap[key];
+        continue loop1;
+      }
+    }
+    newMap[key] = oldMap[key];
+  }
 };
 
 remapFn(rules, _rules);
 remapFn(settings, _settings);
 
 await fs.writeFile(
-	"oxlintrc.json",
-	JSON.stringify(
-		{
-			env,
-			globals,
-			plugins: plugins
-				.filter(
-					(/** @type {string} */ plugin) =>
-						!unsupportedPlugins.includes(plugin),
-				)
-				.map(
-					(/** @type {string} */ plugin) =>
-						remaps.find(
-							(rule) => rule.old === plugin && !rule.skip && rule.replace,
-						)?.replace ?? plugin,
-				),
-			rules: _rules,
-			settings: _settings,
-		},
-		null,
-		2,
-	),
+  'oxlintrc.json',
+  JSON.stringify(
+    {
+      env,
+      globals,
+      plugins: plugins
+        .filter(
+          (/** @type {string} */ plugin) => !unsupportedPlugins.includes(plugin)
+        )
+        .map(
+          (/** @type {string} */ plugin) =>
+            remaps.find(
+              (rule) => rule.old === plugin && !rule.skip && rule.replace
+            )?.replace ?? plugin
+        ),
+      rules: _rules,
+      settings: _settings
+    },
+    null,
+    2
+  )
 );
